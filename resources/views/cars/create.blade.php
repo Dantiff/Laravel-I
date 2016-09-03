77317c3c618652f29dab35baa5e3e41efc70463b
Dan <danielmburu674@gmail.com>
2016-09-03 15:00:52 +0300

    Code for first laravel setup

---
+++ resources/views/cars/create.blade.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Using Forms</title>
+</head>
+<body>
+<h1>Create Registration forms</h1>
+
+<hr/>
+
+{!! Form::open() !!}
+
+
+
+{!! Form::close() !!}
+
+
+</body>
+</html>
